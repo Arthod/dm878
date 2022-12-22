16010d2598ea08cf8acee74a8d0ca0b007abc177
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2022-12-22 20:45:34 +0100

    more stuff

--- US/civilwar.py
+++ US/civilwar.py
@@ -11,6 +11,7 @@ from dash.dependencies import Input, Output
 
 import dash
 from dash import dcc
+import dash_daq as daq
 from dash import html
 
 import utils
@@ -31,6 +32,8 @@ if (not os.path.exists("dataset/cwsac_battles_pp.csv")):
     preprocess()
 
 cwsac_battles = pd.read_csv("dataset/cwsac_battles_pp.csv", encoding='utf-8')
+cwsac_commanders = pd.read_csv("dataset/cwsac_commanders.csv", encoding='utf-8')
+
 cwsac_battles["strength"] = cwsac_battles["strength"].fillna(0)
 cwsac_battles["casualties"] = cwsac_battles["casualties"].fillna(0)
 cwsac_battles["start_date"] = pd.to_datetime(cwsac_battles["start_date"])
@@ -53,8 +56,19 @@ app.layout = html.Div([
         html.Div(className='row', children=[
             html.Div([
                 html.Div([
-                    dcc.Dropdown(["result", "campaign"], "result", id="dropdownPickType"),
-                ], style={'width': '10%', 'display': 'inline-block', 'vertical-align': 'top', 'margin': '1%'}),
+                    html.H3(children="Focus",
+                            style={'textAlign': 'left', 'font-family': 'Roboto, sans-serif', "padding-bottom": "2px"}),
+                    dcc.Dropdown(["result", "campaign", "strength", "state_name", "theater"], "result", id="dropdownType"),
+
+                    html.H3(children="Filters",
+                            style={'textAlign': 'left', 'font-family': 'Roboto, sans-serif', "padding-bottom": "2px"}),
+                    dcc.Dropdown(list(set(cwsac_battles["campaign"])), id="dropdownCampaigns", multi=True, placeholder="Select Campaign(s)",
+                        style={"padding-bottom": "2px"}),
+                    dcc.Dropdown(list(set(cwsac_battles["state_name"])), id="dropdownStates", multi=True, placeholder="Select State(s)"),
+                    dcc.Dropdown(list(set(cwsac_battles["theater"])), id="dropdownTheaters", multi=True, placeholder="Select Theater(s)"),
+                    dcc.Dropdown(list(set(cwsac_commanders["fullname"])), id="dropdownCommanders", multi=True, placeholder="Select Commander(s)"),
+                    daq.ToggleSwitch(id="filterToggleSwitch", value=False),
+                ], style={'width': '20%', 'display': 'inline-block', 'vertical-align': 'top', 'margin': '1%'}),
                 html.Div([
                     dcc.Graph(id="geomap"),
                     dcc.RangeSlider(
@@ -71,28 +85,79 @@ app.layout = html.Div([
                             1865 + (1/12): "1865-Jan", maxtime: "1865-Apr"
                         }
                     ),
-                ], style={'width': '76%', 'display': 'inline-block', 'vertical-align': 'top', 'margin': '1%'}),
-                html.Div([
-                    dcc.Graph(id="table"),
-                    
-                ], style={'width': '10%', 'display': 'inline-block', 'vertical-align': 'top', 'margin': '1%'})
+                    html.Div(id="placeholder", style={"display": "none"}),
+                ], style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'top', 'margin': '1%'}),
             ]),
-        ])
+        ]),
+        html.Div(className='row', children=[
+            html.Div([
+                html.Div([
+                    # Bloodiest Campaigns
+                    dcc.Graph(id="battleCasualtiesByDate")
+                ], style={'width': '33%', 'display': 'inline-block', 'vertical-align': 'top', 'margin': '0%'}),
+                html.Div([   
+                    dcc.Graph(id="campaignCasualtiesByDate")
+
+                ], style={'width': '66%', 'display': 'inline-block', 'vertical-align': 'top', 'margin': '0%'}),
+            ]),
+        ]),
     ]),
 ])
 
 
 
+
+
 @app.callback(
-    Output(component_id='geomap', component_property='figure'),
-    [Input(component_id='time-slider', component_property='value'),
-    Input(component_id='dropdownPickType', component_property='value'),
-    #Input('geomap', 'clickData')
+    [
+        Output(component_id='geomap', component_property='figure'),
+        Output(component_id='battleCasualtiesByDate', component_property='figure'),
+        Output(component_id='campaignCasualtiesByDate', component_property='figure'),
+    ],
+    [
+        Input(component_id='time-slider', component_property='value'),
+        #Input(component_id='geomap', component_property='figure'),
+        Input(component_id='dropdownType', component_property='value'),
+        Input(component_id='dropdownCampaigns', component_property='value'),
+        Input(component_id='dropdownStates', component_property='value'),
+        Input(component_id='dropdownTheaters', component_property='value'),
+        Input(component_id='dropdownCommanders', component_property='value'),
+        Input(component_id="filterToggleSwitch", component_property="value")
     ]
 )
-def update_output(time, downdown_type):
+def update_map(time, downdown_type, current_dropdown_campaigns, current_dropdown_states, current_dropdown_theaters, current_dropdown_commanders, filter_toggle_switch):
 
     mydata = cwsac_battles.copy(deep=True)
+
+    print(current_dropdown_campaigns)
+    print(current_dropdown_states)
+    print(current_dropdown_theaters)
+    print(current_dropdown_commanders)
+
+    if (filter_toggle_switch):
+
+        if (current_dropdown_campaigns is not None and len(current_dropdown_campaigns)):
+            mydata = mydata[(mydata['campaign'].isin(current_dropdown_campaigns))]
+
+        if (current_dropdown_states is not None and len(current_dropdown_states)):
+            mydata = mydata[(mydata['state_name'].isin(current_dropdown_states))]
+
+        if (current_dropdown_theaters is not None and len(current_dropdown_theaters)):
+            mydata = mydata[(mydata['theater'].isin(current_dropdown_theaters))]
+
+        if (current_dropdown_commanders is not None and len(current_dropdown_commanders)):
+            mydata = mydata[
+                (mydata['conf_commander1'].isin(current_dropdown_commanders)) | 
+                (mydata['conf_commander2'].isin(current_dropdown_commanders)) | 
+                (mydata['conf_commander3'].isin(current_dropdown_commanders)) | 
+                (mydata['conf_commander4'].isin(current_dropdown_commanders)) | 
+
+                (mydata['union_commander1'].isin(current_dropdown_commanders)) | 
+                (mydata['union_commander2'].isin(current_dropdown_commanders)) | 
+                (mydata['union_commander3'].isin(current_dropdown_commanders)) | 
+                (mydata['union_commander4'].isin(current_dropdown_commanders))]
+
+
     
     if time != [mintime, maxtime]:
         formatted_time_min_year = int(time[0])
@@ -113,7 +178,7 @@ def update_output(time, downdown_type):
         print(len(mydata))
 
 
-    fig = px.scatter_mapbox(
+    fig1 = px.scatter_mapbox(
         mydata,
         lat="lat",
         lon="long",
@@ -121,24 +186,39 @@ def update_output(time, downdown_type):
         mapbox_style="carto-positron",
         height=600,
         hover_name="battle_name",
+        symbol="campaign",
         
         #size="strength",
         size_max=15,
-        hover_data=["start_date", "end_date", "strength", "casualties"],
+        hover_data=["start_date", "end_date", "strength", "casualties", "theater"],
         zoom=3,
         title="Battle Locations")
 
-    """fig.update_layout(
-        mapbox = {
-            'style': "white-bg",
-            'center': {'lon': -86, 'lat': 35},
-            'zoom': 3, 'layers': [{
-                'source': usa_geojson,
-                'type':'fill', 'below':'traces','color': 'grey', 'opacity' : 0.2}],
-        },
-        margin = {'l':0, 'r':0, 'b':0, 't':0})"""
 
-    return fig
+    fig2 = px.scatter(
+        mydata,
+        x="start_date",
+        y="casualties",
+        color=downdown_type,
+        title="Casualties by date (Theater/Result)",
+        )
+    fig3 = px.scatter(
+        mydata,
+        x="start_date",
+        y="casualties",
+        color="campaign"
+        )
+    fig4 = px.scatter(
+        mydata,
+        x="start_date",
+        y="strength",
+        color="result"
+        )
+
+    return fig1, fig2, fig3
+
+
+
 
 if __name__ == "__main__":  
     app.run_server(debug=True, port=8080)  # Turn off reloader if inside Jupyter
--- US/civilwar_preprocessing.py
+++ US/civilwar_preprocessing.py
@@ -1,38 +1,77 @@
 
 import pandas as pd
 
+us_states = {   # https://gist.github.com/tlancon/9794920a0c3a9990279de704f936050c
+    'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO', 'Connecticut': 'CT', 'Delaware': 'DE', 'District of Columbia': 'DC', 'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID', 'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA', 'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS', 'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ', 'New Mexico': 'NM', 'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH', 'Oklahoma': 'OK', 'Oregon': 'OR', 'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD', 'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virginia': 'VA', 'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'
+}
+us_states = {v: k for k, v in us_states.items()}
+
 def preprocess():
-    f_out = open("dataset/cwsac_battles_pp.csv", "w")
     
     nps_battles = pd.read_csv("dataset/nps_battles.csv", encoding='utf-8')
+    cwsac_campaigns = pd.read_csv("dataset/cwsac_campaigns.csv", encoding='utf-8')
+    cwsac_commanders = pd.read_csv("dataset/cwsac_commanders.csv", encoding='utf-8')
 
-    f_out.write("battle,url,battle_name,other_names,state,locations,campaign,start_date,end_date,operation,assoc_battles,results_text,result,forces_text,strength,casualties_text,casualties,description,preservation,significance,")
-    f_out.write("lat,long\n")
 
-    with open("dataset/cwsac_battles.csv", "r") as f:
-        for i, line in enumerate(f.readlines()):
-            if (i == 0): continue
+    cwsac_battles = pd.read_csv("dataset/cwsac_battles.csv", encoding='utf-8')
 
-            battle_id = line.split(",")[0]
-            print(battle_id)
+    longs = []
+    lats = []
+    theater_names = []
 
-            nps_battle = nps_battles.loc[nps_battles["cwsac_id"] == battle_id]
-            
-            lats = list(nps_battle["lat"])
-            longs = list(nps_battle["long"])
-            print(lats)
-            if (len(lats) and len(longs)):
-                lat = lats[0]
-                long = longs[0]
-            else:
-                lat = 0
-                long = 0
+    conf_commanders = []
+    union_commanders = []
+
+    cwsac_battles["state_name"] = cwsac_battles["state"].apply(lambda x: us_states[x])
+    
+    for idx, row in cwsac_battles.iterrows():
+
+        battle_id = row["battle"]
+        campaign_name = row["campaign"]
+
+        nps_battle = nps_battles.loc[nps_battles["cwsac_id"] == battle_id]
+        cwsac_campaign = cwsac_campaigns.loc[cwsac_campaigns["campaign"] == campaign_name]
+        theater_names.append(list(cwsac_campaign["theater"])[0])
+
+        # Commanders
+        conf_commanders.append([None, None, None, None])
+        union_commanders.append([None, None, None, None])
+
+        conf_idx = 0
+        union_idx = 0
+        cwsac_commanders2 = cwsac_commanders.loc[cwsac_commanders["battle"] == battle_id]
+        for idx, c in cwsac_commanders2.iterrows():
+            if (c[1] == "Confederate"):
+                conf_commanders[-1][conf_idx] = c[2]
+                conf_idx += 1
+            elif (c[1] == "US"):
+                union_commanders[-1][union_idx] = c[2]
+                union_idx += 1
                 
-            line = line.replace("\n", "")
+        lats_ = list(nps_battle["lat"])
+        longs_ = list(nps_battle["long"])
+        if (len(lats_) and len(longs_)): 
+            lats.append(lats_[0])
+            longs.append(longs_[0])
+        else:
+            lats.append(0)
+            longs.append(0)
 
-            f_out.write(f"{line},{lat},{long}\n")
+    cwsac_battles["conf_commander1"] = [c[0] for c in conf_commanders]
+    cwsac_battles["conf_commander2"] = [c[1] for c in conf_commanders]
+    cwsac_battles["conf_commander3"] = [c[2] for c in conf_commanders]
+    cwsac_battles["conf_commander4"] = [c[3] for c in conf_commanders]
 
-    f_out.close()
+    cwsac_battles["union_commander1"] = [c[0] for c in union_commanders]
+    cwsac_battles["union_commander2"] = [c[1] for c in union_commanders]
+    cwsac_battles["union_commander3"] = [c[2] for c in union_commanders]
+    cwsac_battles["union_commander4"] = [c[3] for c in union_commanders]
+        
+
+    cwsac_battles["long"] = longs
+    cwsac_battles["lat"] = lats
+    cwsac_battles["theater"] = theater_names
+    cwsac_battles.to_csv("dataset/cwsac_battles_pp.csv")
 
 if __name__ == "__main__":
     preprocess()
